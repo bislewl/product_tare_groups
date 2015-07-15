a93dfce937407a19742b9a526349d76ed888fd92
Larry Bislew <bislewl@gmail.com>
2015-07-15 14:52:49 -0500

    updated uninstall.sql for removing admin pages

--- uninstall.sql
+++ uninstall.sql
@@ -5,3 +5,5 @@ WHERE configuration_key= 'PRODUCT_TARE_GROUPS_VERSION'
 LIMIT 1;
 DELETE FROM configuration WHERE configuration_group_id = @configuration_group_id AND configuration_group_id <> 0;
 DELETE FROM configuration_group WHERE configuration_group_id = @configuration_group_id AND configuration_group_id <> 0;
+DELETE FROM admin_pages WHERE page_key = 'config_product_tare_groups';
+DELETE FROM admin_pages WHERE page_key = 'catalogTareGroupsBulk';
